6b7042d35e5ccf64b45bfec2b3d19c1d9c3e38c4
DESKTOP-HE8NS3E\NINH <ninhnv15@gmail.com>
2023-04-20 10:44:35 +0700

    implement play sound system

---
+++ GASCore/Systems/SoundSystems/System/PlaySoundSystem.cs
@@ -0,0 +1,18 @@
+﻿namespace GASCore.Systems.SoundSystems.System
+{
+    using GameFoundation.Scripts.Utilities;
+    using GASCore.Systems.SoundSystems.Components;
+    using Unity.Entities;
+
+    public partial class PlaySoundSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities.WithoutBurst().WithChangeFilter<PlaySoundComponent>().ForEach((Entity entity,in PlaySoundComponent playSoundComponent) =>
+            {
+                AudioManager.Instance.StopAllSound(playSoundComponent.SoundAssetPath.Value);
+                AudioManager.Instance.PlaySound(playSoundComponent.SoundAssetPath.Value,playSoundComponent.IsLoop);
+            }).Run();
+        }
+    }
+}
